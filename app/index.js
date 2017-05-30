baa3d81c8fd54c1724cddcfd04d77a27c442e041
Fabian Buitrago <fabianbuitragov@gmail.com>
2017-05-29 19:08:16 -0500

    babel and webpack implementation

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import { Person } from './person';
+import { PeopleList } from './people-list';
+
+class App {
+    static createJusticeLeague() {
+        let batman = new Person('Bruce', 'Wayne');
+        let superman = new Person('Clark', 'Kent');
+        let justiceLeague = new PeopleList([]);
+
+        justiceLeague.addPerson(batman);
+        justiceLeague.addPerson(superman);
+        justiceLeague.printAll();
+    }
+}
+
+App.createJusticeLeague();
